e27551ebd7ee84982bb0daceef5684e6bd5e6f0f
Radoslaw Roszczyk <rroszczyk@users.noreply.github.com>
2022-04-03 09:59:14 +0200

    dodanie logera do naszego serwera

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+from typing import Optional
+
 import fastapi
 import uvicorn
+
+import logging
+import graypy
+
+logger = logging.getLogger()
+
+handler = graypy.GELFTLSHandler('dione', 12201)
+logger.addHandler(handler)
 
 api = fastapi.FastAPI()
 
@@ -10,14 +20,15 @@ def index():
     <body>
     <h1>Witaj świecie z pierwszej aplikacji webowej napisanej w języku Python</h1>
     <a href='/calculate?x=10&y=15'>Test kalkulatora</a>
-    <a href='/calculate?x=10&y=0'>Test błędu kalkulatora</a>    
+    <a href='/calculate?x=10&y=0'>Test błędu kalkulatora</a>
+    <a href='/calculate?x=10&y=15&z=5'>Test kalkulatora</a>        
     </body>
     </html>
     """
     return fastapi.responses.HTMLResponse(content=body)
 
 @api.get("/calculate")
-def calculate(x: int, y: int):
+def calculate(x: int, y: int, z: Optional[int] = None):
     try:
         value = x / y
     except ZeroDivisionError:
@@ -26,9 +37,13 @@ def calculate(x: int, y: int):
             status_code=400
         )
 
+    if z is not None:
+        z = z ** 2
+
     return {
         'x': x,
         'y': y,
+        'z': z,
         'value': value
     }
 
