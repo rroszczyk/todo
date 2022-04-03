f385623da25144c592e6b354f79c7925a7775bb6
Radoslaw Roszczyk <rroszczyk@users.noreply.github.com>
2022-04-03 09:49:14 +0200

    prosta strona www

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
+import fastapi
+import uvicorn
 
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+api = fastapi.FastAPI()
 
+@api.get("/")
+def index():
+    body = """
+    <html>
+    <body>
+    <h1>Witaj świecie z pierwszej aplikacji webowej napisanej w języku Python</h1>
+    <a href='/calculate?x=10&y=15'>Test kalkulatora</a>
+    <a href='/calculate?x=10&y=0'>Test błędu kalkulatora</a>    
+    </body>
+    </html>
+    """
+    return fastapi.responses.HTMLResponse(content=body)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+@api.get("/calculate")
+def calculate(x: int, y: int):
+    try:
+        value = x / y
+    except ZeroDivisionError:
+        return fastapi.responses.HTMLResponse(
+            content="<html><body>Błąd dzielenia przez zero</body></html>",
+            status_code=400
+        )
 
+    return {
+        'x': x,
+        'y': y,
+        'value': value
+    }
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    uvicorn.run(api, port=8000, host="127.0.0.1")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
