74dbb904b21827af28b8528d4d4042c2724e4376
Radoslaw Roszczyk <rroszczyk@users.noreply.github.com>
2022-04-03 16:03:48 +0200

    finalna wersja projektu BIŚ PK 2022

--- main.py
+++ main.py
@@ -46,44 +46,44 @@ api.include_router(todo_router, prefix="/todo")
 
 # api.router.prefix = "/api/v1"
 
-@api.get("/")
-def index():
-    body = """
-    <html>
-    <body>
-    <h1>Witaj świecie z pierwszej aplikacji webowej napisanej w języku Python</h1>
-    <a href='/calculate?x=10&y=15'>Test kalkulatora</a>
-    <a href='/calculate?x=10&y=0'>Test błędu kalkulatora</a>
-    <a href='/calculate?x=10&y=15&z=5'>Test kalkulatora</a>        
-    </body>
-    </html>
-    """
-    return fastapi.responses.HTMLResponse(content=body)
+# @api.get("/")
+# def index():
+#     body = """
+#     <html>
+#     <body>
+#     <h1>Witaj świecie z pierwszej aplikacji webowej napisanej w języku Python</h1>
+#     <a href='/calculate?x=10&y=15'>Test kalkulatora</a>
+#     <a href='/calculate?x=10&y=0'>Test błędu kalkulatora</a>
+#     <a href='/calculate?x=10&y=15&z=5'>Test kalkulatora</a>
+#     </body>
+#     </html>
+#     """
+#     return fastapi.responses.HTMLResponse(content=body)
 
-@api.get("/calculate")
-def calculate(x: int, y: int, z: Optional[int] = None):
-    '''
-    Kalkulator liczący wynik z dzielenia x przez y
-    '''
-    try:
-        value = x / y
-    except ZeroDivisionError:
-        return fastapi.responses.HTMLResponse(
-            content="<html><body>Błąd dzielenia przez zero</body></html>",
-            status_code=400
-        )
-
-    if z is not None:
-        z = z ** 2
-
-    logger.info("Wykonane zostały obliczenia w metodzie calculate")
-
-    return {
-        'x': x,
-        'y': y,
-        'z': z,
-        'value': value
-    }
+# @api.get("/calculate")
+# def calculate(x: int, y: int, z: Optional[int] = None):
+#     '''
+#     Kalkulator liczący wynik z dzielenia x przez y
+#     '''
+#     try:
+#         value = x / y
+#     except ZeroDivisionError:
+#         return fastapi.responses.HTMLResponse(
+#             content="<html><body>Błąd dzielenia przez zero</body></html>",
+#             status_code=400
+#         )
+#
+#     if z is not None:
+#         z = z ** 2
+#
+#     logger.info("Wykonane zostały obliczenia w metodzie calculate")
+#
+#     return {
+#         'x': x,
+#         'y': y,
+#         'z': z,
+#         'value': value
+#     }
 
 if __name__ == '__main__':
     uvicorn.run(api, port=8000, host="127.0.0.1")
